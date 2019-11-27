dd44e3d7f66475647fa9f33f60393569f6898e58
amatranga <amatranga@mesirowfinancial.com>
2019-11-27 12:52:33 -0600

    Initial
    Initial project setup

---
+++ webpack.config.babel.js
@@ -0,0 +1,33 @@
+import webpack from 'webpack';
+import path from 'path';
+
+const config = {
+  entry: './client/src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public/dist')
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: path.join(__dirname, 'client/src'),
+        exclude: '/node_modules',
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [ '@babel/preset-react', '@babel/preset-env' ]
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.js', '.jsx' ]
+  } 
+};
+
+export default config;
